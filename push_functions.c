67cadf0c59de5811220f638b951ae6e47df3c564
Renata2202 <renata_ag@hotmail.com>
2024-09-28 20:19:34 +0100

    norminette

--- push_functions.c
+++ push_functions.c
@@ -6,56 +6,60 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:27:08 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:27:17 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 20:13:20 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void push_top(t_stack **src, t_stack **dest)
+void	push_top(t_stack **src, t_stack **dest)
 {
-    t_node *temp;
-    if ((*src)->top != NULL)
-    {
-        temp = (*src)->top;
-        (*src)->top = (*src)->top->next;
-        temp->next = (*dest)->top;
-        (*dest)->top = temp;
-        (*dest)->size++;
-        (*src)->size--;
-    }
-}
-void    pa(t_stack **a, t_stack **b)
-{
-    push_top(b, a);
-    ft_printf("pa\n");
-}
-void    pb(t_stack **a, t_stack **b)
-{
-    push_top(a, b);
-    ft_printf("pb\n");
-}
-int append(t_stack *stack, int value)
-{
-    t_node *new_node;
-    t_node *current;
+	t_node	*temp;
 
-    new_node = create_node(value);
-    if (!new_node)
-        return 0;
-    if (stack->top == NULL)
-    {
-        stack->top = new_node;
-    }
-    else
-    {
-        current = stack->top;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = new_node;
-    }
-    stack->size++;
-    return 1;
+	if ((*src)->top != NULL)
+	{
+		temp = (*src)->top;
+		(*src)->top = (*src)->top->next;
+		temp->next = (*dest)->top;
+		(*dest)->top = temp;
+		(*dest)->size++;
+		(*src)->size--;
+	}
+}
+
+void	pa(t_stack **a, t_stack **b)
+{
+	push_top(b, a);
+	ft_printf("pa\n");
+}
+
+void	pb(t_stack **a, t_stack **b)
+{
+	push_top(a, b);
+	ft_printf("pb\n");
+}
+
+int	append(t_stack *stack, int value)
+{
+	t_node	*new_node;
+	t_node	*current;
+
+	new_node = create_node(value);
+	if (!new_node)
+		return (0);
+	if (stack->top == NULL)
+	{
+		stack->top = new_node;
+	}
+	else
+	{
+		current = stack->top;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	stack->size++;
+	return (1);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,57 +6,61 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:27:44 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:42:53 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 20:17:49 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_stack *a;
-    t_stack *b;
-    if (argc < 2)
-        return 0;
-    a = initialize_stack(argc, argv);
-    if (a == NULL)
-        return 1;
-    b = create_stack();
-    if (b == NULL)
-    {
-        free_stack(a);
-        return 1;
-    }
-    if (is_sorted_asc(a))
-    {
-        free_stack(b);
-        free_stack(a);
-        return 0;
-    }
-    if (get_stack_size(a) <= 3)
-    {
-        manage_small_stack(&a);
-        free_stack(b);
-        free_stack(a);
-        return 0;
-    }
-    fill_target_position(&a);
-    manage_big_stack(&a, &b);
-    free_stack(a);
-    free_stack(b);
-    return 0;
+	t_stack	*a;
+	t_stack	*b;
+
+	if (argc < 2)
+		return (0);
+	a = initialize_stack(argc, argv);
+	if (a == NULL)
+		return (1);
+	b = create_stack();
+	if (b == NULL)
+	{
+		free_stack(a);
+		return (1);
+	}
+	if (is_sorted_asc(a))
+	{
+		free_stack(b);
+		free_stack(a);
+		return (0);
+	}
+	if (get_stack_size(a) <= 3)
+	{
+		manage_small_stack(&a);
+		free_stack(b);
+		free_stack(a);
+		return (0);
+	}
+	fill_target_position(&a);
+	manage_big_stack(&a, &b);
+	free_stack(a);
+	free_stack(b);
+	return (0);
 }
-void print_stack(t_stack *stack)
+
+void	print_stack(t_stack *stack)
 {
-    if (stack == NULL)
-    {
-        printf("stack is NULL\n");
-        return;
-    }
-    t_node *current = stack->top;
-    while (current != NULL)
-    {
-        printf("%d\n",current->value);
-        current = current->next;
-    }
+	t_node	*current;
+
+	if (stack == NULL)
+	{
+		printf("stack is NULL\n");
+		return ;
+	}
+	current = stack->top;
+	while (current != NULL)
+	{
+		printf ("%d\n",current->value);
+		current = current->next;
+	}
 }
