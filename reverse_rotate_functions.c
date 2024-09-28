6161dfe7a2f492cd659b5ac1a0223e259e1ba010
Renata2202 <renata_ag@hotmail.com>
2024-09-28 20:43:55 +0100

    norminette

--- reverse_rotate_functions.c
+++ reverse_rotate_functions.c
@@ -6,44 +6,47 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:32:44 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:32:53 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 20:43:17 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void reverse_rotate(t_stack **stack)
+void	reverse_rotate(t_stack **stack)
 {
-    t_node *prev;
-    t_node *current;
+	t_node	*prev;
+	t_node	*current;
 
-    prev = NULL;
-    current = (*stack)->top;
-    while (current->next != NULL)
-    {
-        prev = current;
-        current = current->next;
-    }
-    if (prev != NULL)
-    {
-        prev->next = NULL;
-    }
-    current->next = (*stack)->top;
-    (*stack)->top = current;
+	prev = NULL;
+	current = (*stack)->top;
+	while (current->next != NULL)
+	{
+		prev = current;
+		current = current->next;
+	}
+	if (prev != NULL)
+	{
+		prev->next = NULL;
+	}
+	current->next = (*stack)->top;
+	(*stack)->top = current;
 }
-void rra(t_stack **a)
+
+void	rra(t_stack **a)
 {
-    reverse_rotate(a);
-    ft_printf("rra\n");
+	reverse_rotate(a);
+	ft_printf("rra\n");
 }
-void rrb(t_stack **b)
+
+void	rrb(t_stack **b)
 {
-    reverse_rotate(b);
-    ft_printf("rrb\n");
+	reverse_rotate(b);
+	ft_printf("rrb\n");
 }
-void rrr(t_stack **a, t_stack **b)
+
+void	rrr(t_stack **a, t_stack **b)
 {
-    reverse_rotate(a);
-    reverse_rotate(b);
-    ft_printf("rrr\n");
+	reverse_rotate(a);
+	reverse_rotate(b);
+	ft_printf("rrr\n");
 }
