55bd69f0757dfe7deadd45cf529fa202806886ed
Renata2202 <renata_ag@hotmail.com>
2024-09-28 21:18:08 +0100

    norminette

--- stack_functions.c
+++ stack_functions.c
@@ -6,109 +6,116 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:22:16 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:37:25 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 21:14:12 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_node  *create_node(int value)
+t_node	*create_node(int value)
 {
-    t_node *new_node;
+	t_node	*new_node;
 
-    new_node = (t_node *)malloc(sizeof(t_node));
-    if (new_node == NULL)
-    {
-         write (2, "Error\n", 6);
-        return NULL;
-    }
-    new_node->value = value;
-    new_node->position = 0;
-    new_node->cost_a = 0;
-    new_node->cost_b = 0;
-    new_node->a_goal_position = 0;
-    new_node->target_position = 0;
-    new_node->next = NULL;
-    return new_node;
+	new_node = (t_node *)malloc(sizeof(t_node));
+	if (new_node == NULL)
+	{
+		write (2, "Error\n", 6);
+		return (NULL);
+	}
+	new_node->value = value;
+	new_node->position = 0;
+	new_node->cost_a = 0;
+	new_node->cost_b = 0;
+	new_node->a_goal_position = 0;
+	new_node->target_position = 0;
+	new_node->next = NULL;
+	return (new_node);
 }
-t_stack *create_stack()
-{
-    t_stack *stack;
 
-    stack = (t_stack *)malloc(sizeof(t_stack));
-    if (stack == NULL)
-    {
-        write (2, "Error\n", 6);
-        return NULL;
-    }
-    stack->top = NULL;
-    stack->last = NULL;
-    stack->size = 0;
-    return stack;
-}
-int add_to_stack(t_stack *stack, char *arg)
+t_stack	*create_stack()
 {
-    int value;
-    if (!is_valid_input(arg))
-        return 0;
-    value = ft_atoi(arg);
-    if (has_repeated_numbers(stack, value))
-    {
-        write(2,"Error\n", 6);
-        return 0;
-    }
-    t_node *new_node = create_node(value);
-    if (new_node == NULL)
-    {
-        write(2,"Error\n", 6);
-        return 0;
-    }
-    if (stack->top == NULL)
-    {
-        stack->top = new_node;
-        stack->last = new_node;
-    }
-    else
-    {
-        stack->last->next = new_node;
-        stack->last = new_node;
-    }
-    stack->size++;
-    return 1;
-}
-t_stack *initialize_stack(int argc, char **argv)
-{
-    int i;
-    t_stack *stack;
+	t_stack	*stack;
 
-    i = 1;
-    stack = create_stack();
-    if ((argc < 2) || (!stack))
-    {
-        write (2, "Error\n", 6);
-        return NULL;
-    }
-    while (i < argc)
-    {
-        if (!add_to_stack(stack, argv[i]))
-        {
-            free_stack(stack);
-            return NULL;
-        }
-        i++;
-    }
-    return stack;
+	stack = (t_stack *)malloc(sizeof(t_stack));
+	if (stack == NULL)
+	{
+		write (2, "Error\n", 6);
+		return (NULL);
+	}
+	stack->top = NULL;
+	stack->last = NULL;
+	stack->size = 0;
+	return (stack);
 }
-void    free_stack(t_stack *stack)
-{
-    t_node *current = stack->top;
-    t_node *next;
 
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-    free(stack);
+int	add_to_stack(t_stack *stack, char *arg)
+{
+	int		value;
+	t_node	*new_node;
+
+	if (!is_valid_input(arg))
+		return (0);
+	value = ft_atoi(arg);
+	if (has_repeated_numbers(stack, value))
+	{
+		write (2, "Error\n", 6);
+		return (0);
+	}
+	new_node = create_node(value);
+	if (new_node == NULL)
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	if (stack->top == NULL)
+	{
+		stack->top = new_node;
+		stack->last = new_node;
+	}
+	else
+	{
+		stack->last->next = new_node;
+		stack->last = new_node;
+	}
+	stack->size++;
+	return (1);
+}
+
+t_stack	*initialize_stack(int argc, char **argv)
+{
+	int		i;
+	t_stack	*stack;
+
+	i = 1;
+	stack = create_stack();
+	if ((argc < 2) || (!stack))
+	{
+		write (2, "Error\n", 6);
+		return (NULL);
+	}
+	while (i < argc)
+	{
+		if (!add_to_stack(stack, argv[i]))
+		{
+			free_stack(stack);
+			return (NULL);
+		}
+		i++;
+	}
+	return (stack);
+}
+
+void	free_stack(t_stack *stack)
+{
+	t_node	*current;
+	t_node	*next;
+
+	current = stack->top;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	free(stack);
 }
--- swap_functions.c
+++ swap_functions.c
@@ -6,38 +6,42 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:23:15 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:23:25 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 21:16:55 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void swap_top_two(t_stack **stack)
+void	swap_top_two(t_stack **stack)
 {
-    t_node *first;
-    t_node *second;
-    if ((*stack)->top != NULL && (*stack)->top->next != NULL)
-    {
-        first = (*stack)->top;
-        second = (*stack)->top->next;
-        first->next = second->next;
-        second->next = first;
-        (*stack)->top = second;
-    }
+	t_node	*first;
+	t_node	*second;
+
+	if ((*stack)->top != NULL && (*stack)->top->next != NULL)
+	{
+		first = (*stack)->top;
+		second = (*stack)->top->next;
+		first->next = second->next;
+		second->next = first;
+		(*stack)->top = second;
+	}
 }
-void sa(t_stack **a)
+
+void	sa(t_stack **a)
 {
-    swap_top_two(a);
-    ft_printf("sa\n");
+	swap_top_two(a);
+	ft_printf("sa\n");
 }
-void sb(t_stack **b)
+
+void	sb(t_stack **b)
 {
-    swap_top_two(b);
-    ft_printf("sb\n");
+	swap_top_two(b);
+	ft_printf("sb\n");
 }
-void ss(t_stack **a, t_stack **b)
+
+void	ss(t_stack **a, t_stack **b)
 {
-    swap_top_two(a);
-    swap_top_two(b);
-    ft_printf("ss\n");
+	swap_top_two(a);
+	swap_top_two(b);
+	ft_printf("ss\n");
 }
