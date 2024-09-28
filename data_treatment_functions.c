87fd5e2d4ab170e051eecf7413a10074f7978ad0
Renata2202 <renata_ag@hotmail.com>
2024-09-28 20:06:05 +0100

    NORMINETTE

--- data_treatment_functions.c
+++ data_treatment_functions.c
@@ -6,47 +6,50 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:11:05 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 19:33:24 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 19:47:11 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int is_valid_input(const char *input)
+int	is_valid_input(const char *input)
 {
-    while (*input)
-    {
-        if (*input == ' ' || *input == '\n' || *input == '\t' || *input == '"')
-            input++;
-        else if (*input == '-' || *input == '+')
-        {
-            input++;
-            if (!(*input >= '0' && *input <= '9'))
-            return 0;
-        }
-        else if (*input >= '0' && *input <= '9')
-            input++;
-        else
-            return 0;
-    }
-    return 1;
+	while (*input)
+	{
+		if (*input == ' ' || *input == '\n' || *input == '\t' || *input == '"')
+			input++;
+		else if (*input == '-' || *input == '+')
+		{
+			input++;
+			if (!(*input >= '0' && *input <= '9'))
+				return (0);
+		}
+		else if (*input >= '0' && *input <= '9')
+			input++;
+		else
+			return (0);
+	}
+	return (1);
 }
 
-bool has_repeated_numbers(t_stack *stack, int value)
+bool	has_repeated_numbers(t_stack *stack, int value)
 {
-    t_node *current = stack->top;
-    while (current)
-    {
-        if (current->value == value)
-            return true;
-        current = current->next;
-    }
-    return (false);
+	t_node	*current;
+
+	current = stack->top;
+	while (current)
+	{
+		if (current->value == value)
+			return (true);
+		current = current->next;
+	}
+	return (false);
 }
-int absolute_value(int n)
+
+int	absolute_value(int n)
 {
-    if (n < 0)
-        return (-n);
-    else
-        return (n);
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
 }
--- fill_positions_functions.c
+++ fill_positions_functions.c
@@ -6,128 +6,137 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:11:30 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:25:14 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 20:05:30 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void fill_positions(t_stack **a, t_stack **b)
+void	fill_positions(t_stack **a, t_stack **b)
 {
-    t_node *current;
-    int position;
+	t_node	*current;
+	int		position;
 
-    current = (*a)->top;
-    position = 0;
-    while (current)
-    {
-        current->position = position;
-        position++;
-        current = current->next;
-    }
-    current = (*b)->top;
-    position = 0;
-    while (current)
-    {
-        current->position = position;
-        position++;
-        current = current->next;
-    }
+	current = (*a)->top;
+	position = 0;
+	while (current)
+	{
+		current->position = position;
+		position++;
+		current = current->next;
+	}
+	current = (*b)->top;
+	position = 0;
+	while (current)
+	{
+		current->position = position;
+		position++;
+		current = current->next;
+	}
 }
-void fill_target_position(t_stack **stack)
-{
-    t_node *iter_node;
-    t_node *max_node;
-    int biggest;
-    int temp_pos;
 
-    temp_pos = get_stack_size(*stack);
-    while (temp_pos > 0)
-    {
-        iter_node = (*stack)->top;
-        biggest = INT_MIN;
-        max_node = NULL;
-        while (iter_node)
-        {
-            if (iter_node->value > biggest && iter_node->target_position == 0)
-            {
-                biggest = iter_node->value;
-                max_node = iter_node;
-            }
-            iter_node = iter_node->next;
-        }
-        if (max_node)
-            max_node->target_position = temp_pos;
-        temp_pos--;
-    }
+void	fill_target_position(t_stack **stack)
+{
+	t_node	*iter_node;
+	t_node	*max_node;
+	int		biggest;
+	int		temp_pos;
+
+	temp_pos = get_stack_size(*stack);
+	while (temp_pos > 0)
+	{
+		iter_node = (*stack)->top;
+		biggest = INT_MIN;
+		max_node = NULL;
+		while (iter_node)
+		{
+			if (iter_node->value > biggest && iter_node->target_position == 0)
+			{
+				biggest = iter_node->value;
+				max_node = iter_node;
+			}
+			iter_node = iter_node->next;
+		}
+		if (max_node)
+			max_node->target_position = temp_pos;
+		temp_pos--;
+	}
 }
-void get_cost(t_stack **a, t_stack **b)
-{
-    t_node *temp_b;
-    int size_a;
-    int size_b;
 
-    temp_b = (*b)->top;
-    size_a = get_stack_size(*a);
-    size_b = get_stack_size(*b);
-    fill_positions(a, b);
-    while (temp_b)
-    {
-        temp_b->cost_b = temp_b->position;
-        if (temp_b->position > size_b / 2)
-            temp_b->cost_b = (size_b - temp_b->position) * -1;
-        temp_b->a_goal_position = get_target_in_a(a, temp_b->target_position, temp_b->a_goal_position);
-        temp_b->cost_a = temp_b->a_goal_position;
-        if (temp_b->a_goal_position > size_a / 2)
-            temp_b->cost_a = (size_a - temp_b->a_goal_position) * -1;
-        temp_b = temp_b->next;
-    }
+void	get_cost(t_stack **a, t_stack **b)
+{
+	t_node	*temp_b;
+	int		size_a;
+	int		size_b;
+
+	temp_b = (*b)->top;
+	size_a = get_stack_size(*a);
+	size_b = get_stack_size(*b);
+	fill_positions(a, b);
+	while (temp_b)
+	{
+		temp_b->cost_b = temp_b->position;
+		if (temp_b->position > size_b / 2)
+			temp_b->cost_b = (size_b - temp_b->position) * -1;
+		temp_b->a_goal_position = get_target_in_a(a, temp_b->target_position, temp_b->a_goal_position);
+		temp_b->cost_a = temp_b->a_goal_position;
+		if (temp_b->a_goal_position > size_a / 2)
+			temp_b->cost_a = (size_a - temp_b->a_goal_position) * -1;
+		temp_b = temp_b->next;
+	}
 }
-int get_target_in_a(t_stack **a, int b_targ_pos, int a_goal_position)
-{
-    t_node *tmp_a;
-    int target_position = INT_MAX;
 
-    tmp_a = (*a)->top;
-    while (tmp_a)
-    {
-        if (tmp_a->target_position > b_targ_pos && tmp_a->target_position < target_position)
-        {
-            target_position = tmp_a->target_position;
-            a_goal_position = tmp_a->position;
-        }
-        tmp_a = tmp_a->next;
-    }
-    if (target_position != INT_MAX)
-        return a_goal_position;
-    tmp_a = (*a)->top;
-    while (tmp_a)
-    {
-        if (tmp_a->target_position < target_position)
-        {
-            target_position = tmp_a->target_position;
-            a_goal_position = tmp_a->position;
-        }
-        tmp_a = tmp_a->next;
-    }
-    return a_goal_position;
+int	get_target_in_a(t_stack **a, int b_targ_pos, int a_goal_position)
+{
+	t_node	*tmp_a;
+	int		target_position;
+
+	tmp_a = (*a)->top;
+	target_position = INT_MAX;
+	while (tmp_a)
+	{
+		if (tmp_a->target_position > b_targ_pos && tmp_a->target_position < target_position)
+		{
+			target_position = tmp_a->target_position;
+			a_goal_position = tmp_a->position;
+		}
+		tmp_a = tmp_a->next;
+	}
+	if (target_position != INT_MAX)
+		return (a_goal_position);
+	tmp_a = (*a)->top;
+	while (tmp_a)
+	{
+		if (tmp_a->target_position < target_position)
+		{
+			target_position = tmp_a->target_position;
+			a_goal_position = tmp_a->position;
+		}
+		tmp_a = tmp_a->next;
+	}
+	return (a_goal_position);
 }
-int get_min_position(t_stack *stack)
-{
-    t_node *current = stack->top;
-    int min_value = current->value;
-    int min_pos = 0;
-    int pos = 0;
 
-    while (current != NULL)
-    {
-        if (current->value < min_value)
-        {
-            min_value = current->value;
-            min_pos = pos;
-        }
-        current = current->next;
-        pos++;
-    }
-    return min_pos;
+int	get_min_position(t_stack *stack)
+{
+	t_node	*current;
+	int		min_value;
+	int		min_pos;
+	int		pos;
+
+	current = stack->top;
+	min_value = current->value;
+	min_pos = 0;
+	pos = 0;
+	while (current != NULL)
+	{
+		if (current->value < min_value)
+		{
+			min_value = current->value;
+			min_pos = pos;
+		}
+		current = current->next;
+		pos++;
+	}
+	return (min_pos);
 }
