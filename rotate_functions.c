a016e376e5d80095dc2bd9c1890801b763ca5c05
Renata2202 <renata_ag@hotmail.com>
2024-09-28 21:05:03 +0100

    norminette

--- rotate_functions.c
+++ rotate_functions.c
@@ -6,42 +6,45 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:33:17 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:33:27 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 20:45:16 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void rotate(t_stack **stack)
+void	rotate(t_stack **stack)
 {
-    t_node *first;
-    t_node *last;
+	t_node	*first;
+	t_node	*last;
 
-    first = (*stack)->top;
-    last = (*stack)->top;
-    if (!stack || !(*stack)->top || !(*stack)->top->next)
-        return;
-    while (last->next != NULL)
-    {
-        last = last->next;
-    }
-    (*stack)->top = first->next;
-    first->next = NULL;
-    last->next = first;
+	first = (*stack)->top;
+	last = (*stack)->top;
+	if (!stack || !(*stack)->top || !(*stack)->top->next)
+		return ;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	(*stack)->top = first->next;
+	first->next = NULL;
+	last->next = first;
 }
-void ra(t_stack **a)
+
+void	ra(t_stack **a)
 {
-    rotate(a);
-    ft_printf("ra\n");
+	rotate(a);
+	ft_printf("ra\n");
 }
-void rb(t_stack **b)
+
+void	rb(t_stack **b)
 {
-    rotate(b);
-    ft_printf("rb\n");
+	rotate(b);
+	ft_printf("rb\n");
 }
-void rr(t_stack **a, t_stack **b)
+
+void	rr(t_stack **a, t_stack **b)
 {
-    rotate(a);
-    rotate(b);
-    ft_printf("rr\n");
+	rotate(a);
+	rotate(b);
+	ft_printf("rr\n");
 }
--- sort_stack_functions.c
+++ sort_stack_functions.c
@@ -6,89 +6,99 @@
 /*   By: rnunes-a <rnunes-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:33:46 by rnunes-a          #+#    #+#             */
-/*   Updated: 2024/09/28 18:34:20 by rnunes-a         ###   ########.fr       */
+/*   Updated: 2024/09/28 21:04:08 by rnunes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void manage_small_stack(t_stack **stack)
+void	manage_small_stack(t_stack **stack)
 {
-    if (is_sorted_asc(*stack))
-        return;
-    int size = get_stack_size(*stack);
-    if (size == 2)
-        sa(stack);
-    else if (get_stack_size(*stack) == 3)
-    {
-        sort_three_asc(stack);
-        return;
-    }
+	int	size;
+
+	if (is_sorted_asc(*stack))
+		return ;
+	size = get_stack_size(*stack);
+	if (size == 2)
+		sa(stack);
+	else if (get_stack_size(*stack) == 3)
+	{
+		sort_three_asc(stack);
+		return ;
+	}
 }
-void sort_three_asc(t_stack **stack)
+
+void	sort_three_asc(t_stack **stack)
 {
-    t_node *first = (*stack)->top;
-    t_node *second = first->next;
-    t_node *third = second->next;
-    if (*stack == NULL || (*stack)->top == NULL || (*stack)->top->next == NULL || (*stack)->top->next->next == NULL)
-        return;
-    if (first->value < second->value && first->value < third->value && second->value > third->value)
-    {
-        sa(stack);
-        ra(stack);
-    }
-    else if (first->value < second->value && first->value > third->value && second->value > third->value)
-        rra(stack);
-    else if (first->value > second->value && first->value < third->value && second->value < third->value)
-        sa(stack);
-    else if (first->value > second->value && first->value > third->value && second->value > third->value)
-    {
-        sa(stack);
-        rra(stack);
-    }
-    else if (first->value > second->value && first->value > third->value && second->value < third->value)
-        ra(stack);
-    return;
+	t_node	*first;
+	t_node	*second;
+	t_node	*third;
+
+	first = (*stack)->top;
+	second = first->next;
+	third = second->next;
+	if (first->value < second->value && first->value < third->value && second->value > third->value)
+	{
+		sa(stack);
+		ra(stack);
+	}
+	else if (first->value < second->value && first->value > third->value && second->value > third->value)
+		rra(stack);
+	else if (first->value > second->value && first->value < third->value && second->value < third->value)
+		sa(stack);
+	else if (first->value > second->value && first->value > third->value && second->value > third->value)
+	{
+		sa(stack);
+		rra(stack);
+	}
+	else if (first->value > second->value && first->value > third->value && second->value < third->value)
+		ra(stack);
+	return ;
 }
-bool is_sorted_asc(t_stack *stack)
+
+bool	is_sorted_asc(t_stack *stack)
 {
-    t_node *current = stack->top;
-    while (current != NULL && current->next != NULL)
-    {
-        if (current->value > current->next->value)
-            return false;
-        current = current->next;
-    }
-    return true;
+	t_node	*current;
+
+	current = stack->top;
+	while (current != NULL && current->next != NULL)
+	{
+		if (current->value > current->next->value)
+			return (false);
+		current = current->next;
+	}
+	return (true);
 }
-void manage_big_stack(t_stack **stack_a, t_stack **stack_b)
+
+void	manage_big_stack(t_stack **stack_a, t_stack **stack_b)
 {
-    send_values_to_b(stack_a, stack_b);
-    while ((*stack_b)->top != NULL)
-    {
-        get_cost(stack_a, stack_b);
-        do_cheapest_move(stack_a, stack_b);
-    }
-    while (!is_sorted_asc(*stack_a))
-    {
-        if ((*stack_a)->top->target_position > ((get_stack_size(*stack_a))/2))
-            ra(stack_a);
-        else
-            rra(stack_a);
-    }
+	send_values_to_b(stack_a, stack_b);
+	while ((*stack_b)->top != NULL)
+	{
+		get_cost(stack_a, stack_b);
+		do_cheapest_move(stack_a, stack_b);
+	}
+	while (!is_sorted_asc(*stack_a))
+	{
+		if ((*stack_a)->top->target_position > ((get_stack_size (*stack_a)) / 2))
+			ra(stack_a);
+		else
+			rra(stack_a);
+	}
 }
-int process_stacks(t_stack *a, t_stack *b)
+
+int	process_stacks(t_stack *a, t_stack *b)
 {
-    if (get_stack_size(a) <= 3)
-    {
-        manage_small_stack(&a);
-        free_stack(a);
-        free_stack(b);
-        return 0;
-    }
-    fill_target_position(&a);
-    manage_big_stack(&a, &b);
-    free_stack(a);
-    free_stack(b);
-    return 0;
+	if (get_stack_size(a) <= 3)
+	{
+		manage_small_stack(&a);
+		free_stack(a);
+		free_stack(b);
+		return (0);
+	}
+	fill_target_position(&a);
+	manage_big_stack(&a, &b);
+	free_stack(a);
+	free_stack(b);
+	return (0);
 }
